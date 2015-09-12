d725027b6148e7176f7b83e1517ee8411334e7ab
pupboss <pupboss.i@gmail.com>
2015-09-12 17:39:10 +0800

    change indentation.

--- eduadmin/Class/Schedule/View/OldClassTableView.m
+++ eduadmin/Class/Schedule/View/OldClassTableView.m
@@ -68,83 +68,82 @@
 - (void)setDict:(NSDictionary *)dict {
     
     NSDictionary *courses = dict[@"courses"];
-    
 
-        self.s1_1.text = courses[@"1-1"];
+    self.s1_1.text = courses[@"1-1"];
 
-        self.s1_2.text = courses[@"1-2"];
+    self.s1_2.text = courses[@"1-2"];
 
-        self.s1_3.text = courses[@"1-3"];
+    self.s1_3.text = courses[@"1-3"];
 
-        self.s1_4.text = courses[@"1-4"];
+    self.s1_4.text = courses[@"1-4"];
 
-        self.s1_5.text = courses[@"1-5"];
-
-    
-        self.s2_1.text = courses[@"2-1"];
-
-        self.s2_2.text = courses[@"2-2"];
-
-        self.s2_3.text = courses[@"2-3"];
-
-        self.s2_4.text = courses[@"2-4"];
- 
-        self.s2_5.text = courses[@"2-5"];
+    self.s1_5.text = courses[@"1-5"];
 
 
-        self.s3_1.text = courses[@"3-1"];
+    self.s2_1.text = courses[@"2-1"];
 
-        self.s3_2.text = courses[@"3-2"];
+    self.s2_2.text = courses[@"2-2"];
 
-        self.s3_3.text = courses[@"3-3"];
+    self.s2_3.text = courses[@"2-3"];
 
-        self.s3_4.text = courses[@"3-4"];
+    self.s2_4.text = courses[@"2-4"];
 
-        self.s3_5.text = courses[@"3-5"];
-
-    
-        self.s4_1.text = courses[@"4-1"];
-
-        self.s4_2.text = courses[@"4-2"];
-
-        self.s4_3.text = courses[@"4-3"];
-
-        self.s4_4.text = courses[@"4-4"];
-
-        self.s4_5.text = courses[@"4-5"];
+    self.s2_5.text = courses[@"2-5"];
 
 
-        self.s5_1.text = courses[@"5-1"];
+    self.s3_1.text = courses[@"3-1"];
 
-        self.s5_2.text = courses[@"5-2"];
+    self.s3_2.text = courses[@"3-2"];
 
-        self.s5_3.text = courses[@"5-3"];
+    self.s3_3.text = courses[@"3-3"];
 
-        self.s5_4.text = courses[@"5-4"];
+    self.s3_4.text = courses[@"3-4"];
 
-        self.s5_5.text = courses[@"5-5"];
-
-
-        self.s6_1.text = courses[@"6-1"];
-
-        self.s6_2.text = courses[@"6-2"];
-
-        self.s6_3.text = courses[@"6-3"];
-
-        self.s6_4.text = courses[@"6-4"];
-
-        self.s6_5.text = courses[@"6-5"];
+    self.s3_5.text = courses[@"3-5"];
 
 
-        self.s7_1.text = courses[@"7-1"];
+    self.s4_1.text = courses[@"4-1"];
 
-        self.s7_2.text = courses[@"7-2"];
+    self.s4_2.text = courses[@"4-2"];
 
-        self.s7_3.text = courses[@"7-3"];
+    self.s4_3.text = courses[@"4-3"];
 
-        self.s7_4.text = courses[@"7-4"];
-    
-        self.s7_5.text = courses[@"7-5"];
+    self.s4_4.text = courses[@"4-4"];
+
+    self.s4_5.text = courses[@"4-5"];
+
+
+    self.s5_1.text = courses[@"5-1"];
+
+    self.s5_2.text = courses[@"5-2"];
+
+    self.s5_3.text = courses[@"5-3"];
+
+    self.s5_4.text = courses[@"5-4"];
+
+    self.s5_5.text = courses[@"5-5"];
+
+
+    self.s6_1.text = courses[@"6-1"];
+
+    self.s6_2.text = courses[@"6-2"];
+
+    self.s6_3.text = courses[@"6-3"];
+
+    self.s6_4.text = courses[@"6-4"];
+
+    self.s6_5.text = courses[@"6-5"];
+
+
+    self.s7_1.text = courses[@"7-1"];
+
+    self.s7_2.text = courses[@"7-2"];
+
+    self.s7_3.text = courses[@"7-3"];
+
+    self.s7_4.text = courses[@"7-4"];
+
+    self.s7_5.text = courses[@"7-5"];
 }
 
 + (instancetype)newOldClassTable {
